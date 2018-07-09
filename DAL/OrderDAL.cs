78efadc0011da8505162c0e2d12a58c3344ba3a3
LeTruongGiangVTCA0 <gianglt.nde17042@vtc.eud.vn>
2018-07-09 19:57:29 +0700

    Project

--- DAL/OrderDAL.cs
+++ DAL/OrderDAL.cs
@@ -16,17 +16,79 @@ namespace DAL
             MySqlConnection connection = DbConfiguration.OpenConnection();
             MySqlCommand cmd = connection.CreateCommand();
             cmd.Connection = connection;
+            try
+            {
             //Khoa cap nhat tat ca table , bao dam tinh toan ven du lieu
             cmd.CommandText = "lock tables Employees write, Orders write, Books write, List_Order_ID write;";
             cmd.ExecuteNonQuery();
             MySqlTransaction trans = connection.BeginTransaction();
             cmd.Transaction = trans;
             MySqlDataReader reader = null;
-            if(order.ID_E == null || order.ID_E.full_name == null || order.ID_E.full_name == "")
+            //Nhập dữ liệu cho bảng Order
+            cmd.CommandText = "insert into Orders(Book_ID,Note) values (@Book_ID, @Note);";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Book_ID",order.OrderBooks.Book_ID);
+            cmd.Parameters.AddWithValue("@Note",OrdersNote.CREATE_NEW_ORDER);
+            cmd.ExecuteNonQuery();
+            //Lấy Order_ID mới 
+            cmd.CommandText = "select LAST_INSERT_ID() as ID_Order");
+             if (reader.Read())
+                {
+                    order.OrderId = reader.GetInt32("order_id");
+                }
+                reader.Close();
+            //Nhập vào bảng 
+             foreach (var item in order.ItemsList)
+                {
+                    if (Books.Book_ID == null || Books.Amount <= 0)
+                    {
+                        throw new Exception("Not Exists Item");
+                    }
+            //Lấy unit_price Order_Details
+            cmd.CommandText = "select unit_price from where Book_ID=@Book_ID";
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Book_ID",Books.Book_ID);
+            reader = cmd.ExecuteReader();
+             if (!reader.Read())
+                    {
+                        throw new Exception("Not Exists Item");
+                    }
+                    item.ItemPrice = reader.GetDecimal("unit_price");
+                    reader.Close();
+        
+        //Nhập dữ liệu vào bảng Order_Details
+         cmd.CommandText = @"insert into OrderDetails(ID_order,ID_Book,unit_price, quantity) values
+                            (" Orders.ID_order + "," + Books.Book_ID + "," + Books.unit_price + "," + Books.Amount + ");";
+         cmd.ExecuteNonQuery();
+         //Cập nhập số lượng mới của sách
+         cmd.CommandText = "update Books set amount=amount-@quantity where Books_ID=" + Books.Book_ID + ";";
+          cmd.Parameters.Clear();
+          cmd.Parameters.AddWithValue("@quantity", Books.Amount);
+          cmd.ExecuteNonQuery();
+          }
+          //commit transaction
+                trans.Commit();
+                result = true;
+        }   
+            catch (Exception ex)
             {
-                
+                  Console.WriteLine(ex.Message);
+                result = false;
+                try
+                {
+                    trans.Rollback();
+                }
+                catch
+                {
+                }
             }
-            
-        }
+             finally
+            {   
+                //Mở khóa tất cả các bảng
+                cmd.CommandText = "unlock tables;";
+                cmd.ExecuteNonQuery();
+                DBHelper.CloseConnection();
+            }
+            return result;
     }
 }
--- DataBase.sql
+++ DataBase.sql
@@ -15,21 +15,23 @@ User_Password varchar(225) not null
 create table Books(
 ID_Book int(11) auto_increment , constraint PK_ID_Book primary key(ID_Book),
 book_title varchar(225) not null,
-author varchar(225) not null,
+unit_price decimal(20,2) default 0,
 amount int(10) not null,
 price decimal not null
 );
 
 create table Orders(
 ID_Order int(11) auto_increment , constraint PK_ID_Order primary key(ID_Order),
+ID_Book int(11),
 ID_E int(11) , constraint FK_ID_E foreign key(ID_E) references Employee(ID_E),
 Creation_Time datetime default now(),
 Note varchar(225)
 );
 
-create table List_order_ID(
+create table OrderDetails(
 ID_order int(11), constraint FK_ID_Order foreign key(ID_Order) references Orders(ID_Order),
 ID_Book int(11), constraint FK_ID_Book foreign key (ID_Book) references Books(ID_book),
-Amount int(10) not null
+unit_price decimal(20,2) default 0,
+quantity int not null default 1
 );
 
