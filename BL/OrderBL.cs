02428180ccde782ce65b5eeb62658f7d36e56dd4
LeTruongGiangVTCA0 <gianglt.nde17042@vtc.eud.vn>
2018-07-12 16:11:39 +0700

    Project

--- BL/OrderBL.cs
+++ BL/OrderBL.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using Persistence;
+using DAL;
 
 namespace BL
 {
     public class OrderBL
     {
-            
-    }
-}
+        private OrderDAL O_BL = new OrderDAL();
+        public bool AddOrder(Orders order)
+        {
+            bool result = O_BL.AddOrder(order);
