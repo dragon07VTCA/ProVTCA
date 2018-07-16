c4229b0b1a808ec0056e8448d892e73a85bdeaeb
dragon07VTCA <truongdx.nde17056@vtc.edu.vn>
2018-07-16 15:11:28 +0700

    Project

--- BL/OrderBL.cs
+++ BL/OrderBL.cs
@@ -7,7 +7,5 @@ namespace BL
 {
     public class OrderBL
     {
-        private OrderDAL O_BL = new OrderDAL();
-        public bool AddOrder(Orders order)
-        {
-            bool result = O_BL.AddOrder(order);
+    }
+}
--- PL_Console/Program.cs
+++ PL_Console/Program.cs
@@ -1,14 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using Persistence;
-using BL;
+using static System.Net.Mime.MediaTypeNames;
 namespace PL_Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Menu menu = new Menu();
+            menu.Menu0();
         }
     }
+    public class Menu
+    {
+        public void Menu0()
+        {
+            BL.EmployeesBL employee = new BL.EmployeesBL();
+            Console.WriteLine("---Chao mung den voi cua hang sach TG---");
+            Console.WriteLine("=========================================\n");
+            Console.WriteLine("1. Dang nhap he thong");
+            Console.WriteLine("2. Exit\n");
+            for (; ; )
+            {
+                Console.Write("#Chon: ");
+                string select = Console.ReadLine();
+                switch (select)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Dang nhap he thong:");
+                        Console.WriteLine("======================================");
+                        for (; ; )
+                        {
+
+                            Console.Write("-Ten dang nhap: ");
+                            string user = Console.ReadLine();
+                            Console.Write("-Mat khau: ");
+                            string password = Console.ReadLine();
+                            employee.GetEmployeeByUserPassword(user, password);
+                            if (employee.GetEmployeeByUserPassword(user,password) != null)
+                            {
+                                Console.WriteLine("gafsfsdafgsgsdg");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("sai r r r r");
+                            }
+                        }
+                        break;
+                    case "2":
+                        break;
+                    default:
+                        Console.WriteLine("- Ky tu ban nhap khong dung ! Vui long thu lai !");
+                        break;
+                }
+                if (select == "2")
+                {
+                    break;
+                }
+            }
+        }
+    }
+
 }
