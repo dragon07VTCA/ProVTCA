6840445a6da9c6a4ad81e4384539286b31e4e2cc
dragon07VTCA <truongdx.nde17056@vtc.edu.vn>
2018-07-19 17:39:45 +0700

    Project

--- PL_Console/Program.cs
+++ PL_Console/Program.cs
@@ -2,6 +2,7 @@
 using Persistence;
 using BL;
 using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PL_Console
 {
@@ -63,14 +64,6 @@ namespace PL_Console
         {
             try
             {
-                EmployeesBL employee = new EmployeesBL();
-                BooksBL book = new BooksBL();
-                OrderBL order = new OrderBL();
-                Orders o = new Orders();
-                Books b = new Books();
-                Employees e = new Employees();
-                OrderDetails od = new OrderDetails();
-                List<OrderDetails> ListOrderDetails = new List<OrderDetails>();
                 Console.Clear();
                 Console.WriteLine("----Chao mung den voi cua hang sach TG----");
                 Console.WriteLine("==========================================\n");
@@ -86,10 +79,15 @@ namespace PL_Console
                             Menu01();
                             break;
                         case "2":
+                            Environment.Exit(0);
                             break;
                         default:
                             Console.WriteLine("- Ky tu ban nhap khong dung ! Vui long thu lai !");
                             break;
+                    }
+                    if (selectNumber == "2")
+                    {
+                        break;
                     }
                 }
             }
@@ -98,8 +96,11 @@ namespace PL_Console
                 Console.WriteLine("- Loi khong xac dinh !!!!");
             }
         }
+        public List<Employees> ListE = new List<Employees>();
         public void Menu01()
         {
+            Employees e = new Employees();
+            EmployeesBL employee = new EmployeesBL();
             Console.Clear();
             Console.WriteLine("Dang nhap he thong:");
             Console.WriteLine("======================================");
@@ -110,36 +111,11 @@ namespace PL_Console
                 Console.Write("-Mat khau: ");
                 string password = Console.ReadLine();
                 e = employee.GetEmployeeByUserPassword(user, password);
+                ListE.Add(e);
                 if (e != null)
                 {
-                    Console.Clear();
-                    Console.WriteLine("                         Cua hang sach TG");
-                    Console.WriteLine("=========================================\n");
-                    Console.WriteLine("1. Tao hoa don");
-                    Console.WriteLine("2. Thanh toan");
-                    Console.WriteLine("3. Dang xuat\n");
-                    Console.Write("#Chon: ");
-                    selectNumber = Console.ReadLine();
-                    for (; ; )
-                    {
-                        switch (selectNumber)
-                        {
-                            case "1":
-                                Menu011();
-                                break;
-                            case "2":
-                                break;
-                            case "3":
-                                Console.WriteLine("----Chao mung den voi cua hang sach TG----");
-                                Console.WriteLine("==========================================\n");
-                                Console.WriteLine("1. Dang nhap he thong");
-                                Console.WriteLine("2. Thoat\n");
-                                break;
-                            default:
-                                Console.WriteLine("- Ky tu ban nhap khong dung ! Vui long nhap lai !");
-                                break;
-                        }
-                    }
+                    Menu00();
+                    break;
                 }
                 else
                 {
@@ -147,45 +123,81 @@ namespace PL_Console
                 }
             }
         }
-        public void Menu011()
+        public void Menu00()
         {
             Console.Clear();
             Console.WriteLine("                         Cua hang sach TG");
+            Console.WriteLine("=========================================\n");
+            Console.WriteLine("1. Tao hoa don");
+            Console.WriteLine("2. Thanh toan");
+            Console.WriteLine("3. Dang xuat\n");
+            Console.Write("#Chon: ");
+            selectNumber = Console.ReadLine();
+            for (; ; )
+            {
+                switch (selectNumber)
+                {
+                    case "1":
+                        Menu011();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        Menu0();
+                        break;
+                    default:
+                        Console.WriteLine("- Ky tu ban nhap khong dung ! Vui long nhap lai !");
+                        break;
+                }
+            }
+        }
+        public void Menu011()
+        {
+            EmployeesBL employee = new EmployeesBL();
+            BooksBL book = new BooksBL();
+            OrderBL order = new OrderBL();
+            Orders o = new Orders();
+            Books b = new Books();
+            OrderDetails od = new OrderDetails();
+            Console.Clear();
+            Console.WriteLine("                         Cua hang sach TG");
             Console.WriteLine("=========================================");
-                for ( ; ; )
-                {                      
-                    int i = 0;
-                    i++;
-                    Console.Write("- Nhap ID Book: ");
-                    od.book.ID_Book = Convert.ToInt32(Console.ReadLine());                        
-                    b = book.GetBookById(od.book.ID_Book);
-                    if (b != null)
+            for (; ; )
+            {
+                o.BooksList = new List<OrderDetails>();
+                o.ID_E = ListE[0];
+                o.creation_time = DateTime.Now;
+                Console.Write("- Nhap ID Book: ");
+                b.ID_Book = Convert.ToInt32(Console.ReadLine());
+                b = book.GetBookById(b.ID_Book);
+                if (b != null)
+                {
+                    od.book.ID_Book = b.ID_Book;
+                    od.book.unit_price = b.unit_price;
+                    Console.Write("- Nhap so luong: ");
+                    od.quantity = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(b.amount);
+                    Console.WriteLine(b.unit_price);
+                    Console.WriteLine("\n=========================================");
+                    Console.Write("Ban co muon tiep tuc mua them sach(Co/Khong): ");
+                    selectChar = Console.ReadLine();
+                    o.BooksList.Add(od);
+                    if (selectChar == "Co" || selectChar == "co")
                     {
-                        Console.Write("- Nhap so luong: ");
-                        int quantity = Convert.ToInt32(Console.ReadLine());
-                        quantity = od.quantity;                            
-                        od.book = b;                                                    
-                        // Console.WriteLine(b.amount);
-                        // Console.WriteLine(b.unit_price);
-                        Console.WriteLine("\n=========================================");
-                        Console.Write("Ban co muon tiep tuc mua them sach(Co/Khong): ");
-                        selectChar = Console.ReadLine();
-                        if (selectChar == "Co" || selectChar == "co")                            
-                        {                                    
-                            Console.Clear();
-                            ListOrderDetails.Add(od);
-                        }
-                        else if (selectChar == "Khong" || selectChar == "khong")
-                        {
-                            order.AddOrder(o);                                
-                            break;
-                        }
+                        order.AddOrder(o);
                     }
-                    else
+                    else if (selectChar == "Khong" || selectChar == "khong")
                     {
-                        Console.WriteLine("- Sach khong ton tai ! Vui long nhap lai ID !");
+                        order.AddOrder(o);
+                        Menu00();
+                        break;
                     }
                 }
+                else
+                {
+                    Console.WriteLine("- Sach khong ton tai ! Vui long nhap lai ID !");
+                }
+            }
         }
     }
 }
